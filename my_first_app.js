a763abb7ea23c923b1b5c9547484a925b5cbffa3
Harsimran Singh <singhharsimranuppal17@gmail.com>
2023-06-30 17:36:00 +0530

    First Commit

---
+++ my_first_app.js
@@ -0,0 +1,12 @@
+console.log("Hey There! Kiddann fir")
+
+// node filename.js ------> is terminal command to run the code
+
+
+//Node.Js is javascript runtime. It has two main features as described below:
+//          1. It makes javascript code run on any computer whereas javascript code can only be executed on browser
+//          2. It also provide additional methods such as interaction with files on computer etc.
+
+// Node.js uses V8 engine  to get executed
+// It is used on server to interact  with database and or to provide more security as code written on server is not accesible by client.
+// Unlike php it does not require any additional tool to listen the requests, Node.js writes the server and listen to the incoming requests.
